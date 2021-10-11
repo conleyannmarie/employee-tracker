c2c18dcaac38a2668362db6ca4fe8ab036674ce1
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-11 17:12:05 -0600

    fixed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,18 @@ CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL,
 )
 
-CREATE TABLE roles (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-
-)
-
-CREATE TABLE manager (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE manager(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER,
-  manager_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(first_name) ON DELETE SET NULL
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES manager(id) ON DELETE SET NULL
 );
